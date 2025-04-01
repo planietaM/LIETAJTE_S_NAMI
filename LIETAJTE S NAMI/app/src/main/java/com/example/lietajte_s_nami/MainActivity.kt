4518aa7871bbfc6b75ed2e2d9fbdd0cd87fabc06
planietaM <planieta4@stud.uniza.sk>
2025-04-01 18:57:18 +0200

    first commit

---
+++ LIETAJTE S NAMI/app/src/main/java/com/example/lietajte_s_nami/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lietajte_s_nami
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lietajte_s_nami.ui.theme.LIETAJTE_S_NAMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LIETAJTE_S_NAMITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LIETAJTE_S_NAMITheme {
+        Greeting("Android")
+    }
+}
